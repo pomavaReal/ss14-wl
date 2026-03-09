ece0bae2d2e83b38508b71af1c9cf4c7ef72c73b
psitheta123 <ss14corvaxenjoyer@gmail.com>
2026-03-09 00:01:12 +0000

    Added component LocatableAnomaly for anomalies and anomaly injectors by which locators are searching (#507)

---
+++ Content.Shared/_WL/Anomaly/Components/LocatableAnomalyComponent.cs
@@ -0,0 +1,8 @@
+using Content.Shared.Anomaly.Effects;
+
+namespace Content.Shared._WL.Anomaly.Components;
+
+[RegisterComponent]
+public sealed partial class LocatableAnomalyComponent : Component
+{
+}
